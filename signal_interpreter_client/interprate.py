4a3e2edfb4da075f8c990ae754929cedfb2661c1
CWeng <51285967+chenyangweng@users.noreply.github.com>
2020-11-17 22:19:32 +0100

    Delete interprate.py
    modified based on Robertos feedback

--- signal_interpreter_client/interprate.py
+++
@@ -1,7 +0,0 @@
-from server_communication_handler import post_message
-from consts import *
-
-def get_interpretation(signal):
-    payload = { SIGNAL: signal }
-    title = post_message(URL, payload)
-    return title
