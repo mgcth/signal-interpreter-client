006c256ef8923ef1a4951f64fb8bd16bdf9a0960
chenyangweng <CWENG@volvocars.com>
2020-12-08 15:07:23 +0100

    add comments

--- signal_interpreter_client/exceptions.py
+++ signal_interpreter_client/exceptions.py
@@ -6,4 +6,6 @@ Custom Exceptions.
 class SignalInterpreterClientError(Exception):
     """
     No signal error.
+    The custom exception could for example be called SignalInterpreterClientError and be used for wrapping other
+    exceptions such as ConnectionError or when the user has entered a signal that is not part of the signal database file
     """
--- tests/integration/test_main.py
+++ tests/integration/test_main.py
@@ -17,13 +17,21 @@ from io import StringIO
 from signal_interpreter_client.interpretation_layer import URL
 from signal_interpreter_client.main import main
 
-
 logger = logging.getLogger(__name__)
 
-
 RESP = "ECU Reset"
+
+
 @responses.activate
 def test_main():
+    """
+    Acceptance criteria
+    Add test cases in signal interpreter client which tests the flow from starting the client from main.py to sending a
+    POST-request and receiving the correct response
+    The integration tests should mock the server and be able to run independently of the signal interpreter server
+    Add the possibility to invoke integration tests in tasks.py but do not check the code coverage for integration tests
+    :return:
+    """
     responses.add(
         responses.POST,
         URL,
